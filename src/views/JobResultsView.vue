b094a70be38a633fcbdedff4a357116e69613e9d
gk1221 <good125628@gmail.com>
2023-03-02 21:26:19 +0800

    jsonserver假後端 文件修改

--- src/views/JobResultsView.vue
+++ src/views/JobResultsView.vue
@@ -1,11 +1,22 @@
 <template>
-  <h1 class="text-6xl font-bold">JobResultsView page</h1>
-  <button @click="goToHome">Go to Home</button>
+  <div class="flex w-full flex-row flex-nowrap">
+    <job-filter-sidebar />
+    <job-listings />
+  </div>
 </template>
 
 <script>
+import JobFilterSidebar from "@/components/JobResults/JobFilterSidebar/JobFilterSidebar.vue";
+import JobListings from "@/components/JobResults/JobListings.vue";
+
 export default {
   name: "JobResultsView",
+
+  components: {
+    JobFilterSidebar,
+    JobListings,
+  },
+
   methods: {
     goToHome() {
       this.$router.push({ name: "Home" });
